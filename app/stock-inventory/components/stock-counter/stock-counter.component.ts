7cc55ebdc5228d6f042f768fcba643bc9e741f9b
Ameet Swamy <ameet79@gmail.com>
2020-01-25 19:48:37 +0530

    control value accessor implementation

--- app/stock-inventory/components/stock-counter/stock-counter.component.ts
+++ app/stock-inventory/components/stock-counter/stock-counter.component.ts
@@ -1,85 +1,111 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const COUNTER_CONTROL_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => StockCounterComponent),
+  multi: true
+};
 
 @Component({
-    selector: 'stock-counter',
-    styles: [`
-        .stock-counter {
-            background: rgba(0, 0, 0, 0.05);
-            border-radius: 3px;
-            overflow: hidden;
-            & > div > div {
-            display: inline-flex;
-            align-items: center;
-        
-            & > div {
-                display: inline-flex;
-                flex-direction: column;
-            }
-            }
-        
-            p {
-            font-size: 16px;
-            font-weight: 400;
-            text-align: center;
-            width: 50px;
-            }
-        
-            button {
-            padding: 3px;
-            border-radius: 0;
-            line-height: 1;
-            padding: 3px 7px;
-            background: #9E61C8;
-            color: #fff;
-            font-weight: 500;
-            &:disabled {
-                background: rgba(0, 0, 0, 0.1);
-                color: rgba(0, 0, 0, 0.4);
-            }
-            }
+  selector: 'stock-counter',
+  providers: [COUNTER_CONTROL_ACCESSOR],
+  styles: [`
+    .stock-counter {
+      background: rgba(0, 0, 0, 0.05);
+      border-radius: 3px;
+      overflow: hidden;
+      & > div > div {
+        display: inline-flex;
+        align-items: center;
+    
+        & > div {
+          display: inline-flex;
+          flex-direction: column;
         }
-    `],
-    template: `
-        <div class="stock-counter">
-            <div>
-                <div>
-                    <div>
-                        <p>{{ value }}</p>
-                        <button 
-                            type="button" 
-                            (click)="increment()"
-                            [disabled]="value === max">
-                            +
-                        </button>
-                        <button 
-                            type="button" 
-                            (click)="decrement()"
-                            [disabled]="value === min">
-                            -
-                        </button>
-                    </div>
-                </div>
-            </div>
+      }
+    
+      p {
+        font-size: 16px;
+        font-weight: 400;
+        text-align: center;
+        width: 50px;
+      }
+    
+      button {
+        padding: 3px;
+        border-radius: 0;
+        line-height: 1;
+        padding: 3px 7px;
+        background: #9E61C8;
+        color: #fff;
+        font-weight: 500;
+        &:disabled {
+          background: rgba(0, 0, 0, 0.1);
+          color: rgba(0, 0, 0, 0.4);
+        }
+      }
+    } 
+  `],
+  template: `
+    <div class="stock-counter">
+      <div>
+        <div>
+          <p>{{ value }}</p>
+          <div>
+            <button 
+              type="button"
+              (click)="increment()"
+              [disabled]="value === max">
+              +
+            </button>
+            <button 
+              type="button"
+              (click)="decrement()"
+              [disabled]="value === min">
+              -
+            </button>
+          </div>
         </div>
-    `
+      </div>
+    </div>
+  `
 })
+export class StockCounterComponent implements ControlValueAccessor {
 
-export class StockCounterComponent {
-    @Input() step: number = 10;
-    @Input() min: number = 10;
-    @Input() max: number = 1000;
+  private onTouch: Function;
+  private onModelChange: Function;
 
-    value: number = 0;
+  registerOnTouched(fn) {
+    this.onTouch = fn;
+  }
+  
+  registerOnChange(fn) {
+    this.onModelChange = fn;
+  }
+  
+  writeValue(value) {
+    this.value = value || 0;
+  }
 
-    increment() {
-        if (this.value < this.max){
-            this.value = this.value + this.step;
-        }
+  @Input() step: number = 10;
+  @Input() min: number = 10;
+  @Input() max: number = 1000;
+
+  value: number = 10;
+
+  increment() {
+    if (this.value < this.max) {
+      this.value = this.value + this.step;
+      this.onModelChange(this.value);
     }
-
-    decrement() {
-        if (this.value > this.min){
-            this.value = this.value - this.step;
-        }
+    this.onTouch();
+  }
+  decrement() {
+    if (this.value > this.min) {
+      this.value = this.value - this.step;
+      this.onModelChange(this.value);
     }
+    this.onTouch();
+  }
 }
--- app/stock-inventory/components/stock-selector/stock-selector.component.ts
+++ app/stock-inventory/components/stock-selector/stock-selector.component.ts
@@ -1,111 +1,52 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core'
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 import { Product } from '../../models/product.interface';
 
-
 @Component({
-    selector: 'stock-selector',
-    styles: [`
-        .stock-selector {
-            padding: 0 0 20px;
-            margin: 0 0 20px;
-            border-bottom: 1px solid #ccc;
-            position: relative;
-        
-            &__error {
-            position: absolute;
-            background: #B52D30;
-            color: #fff;
-            font-weight: 500;
-            font-size: 12px;
-            text-transform: uppercase;
-            border-radius: 3px;
-            left: 0;
-            bottom: -10px;
-            line-height: 1;
-            padding: 6px 10px;
-        
-            &:before {
-                width: 0;
-                height: 0;
-                border-style: solid;
-                border-width: 0 5px 5px 5px;
-                border-color: transparent transparent #B52D30 transparent;
-                content: ' ';
-                display: block;
-                position: absolute;
-                top: -5px;
-                left: 10px;
-            }
-            }
-        
-            & > div {
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-        
-            select {
-                flex: 1 0;
-            }
-            button {
-                flex: 0 0 100px;
-                margin-left: 30px;
-            }
-            stock-counter {
-                flex: 0 0 50px;
-                margin-left: 30px;
-            }
-            }
-        }
-    `],
-    template: `
-        <div class="stock-selector" [formGroup]="parent">
-            <div formGroupName="selector">
-                <select formControlName="product_id">
-                    <option value="">Select stock</option>
-                    <option
-                        *ngFor="let product of products"
-                        [value]="product.id">
-                        {{ product.name }}
-                    </option>
-                </select>
-                <input  
-                    type="number"
-                    step="10"
-                    min="10"
-                    max="1000"
-                    formControlName="quantity">
-                <stock-counter
-                    [step] = "10"
-                    [min] = "10"
-                    [max] = "1000">
-                </stock-counter>
-                <button 
-                    type="button"
-                    (click)="onAdd()">
-                    Add Stock
-                </button>
-            </div>
-        </div>
-    `
+  selector: 'stock-selector',
+  styles: ['stock-selector.component.scss'],
+  template: `
+    <div class="stock-selector" [formGroup]="parent">
+      <div formGroupName="selector">
+        <select formControlName="product_id">
+          <option value="">Select stock</option>
+          <option
+            *ngFor="let product of products"
+            [value]="product.id">
+            {{ product.name }}
+          </option>
+        </select>
+        <stock-counter
+          [step]="10"
+          [min]="10"
+          [max]="1000"
+          formControlName="quantity">
+        </stock-counter>
+        <button 
+          type="button"
+          (click)="onAdd()">
+          Add stock
+        </button>
+      </div>
+    </div>
+  `
 })
-
 export class StockSelectorComponent {
-    @Input()
-    parent: FormGroup;
+  @Input()
+  parent: FormGroup;
+  
+  @Input()
+  products: Product[];
 
-    @Input()
-    products: Product[];
+  @Output()
+  added = new EventEmitter<any>();
 
-    @Output() 
-    added = new EventEmitter<any>();
-
-    onAdd() {
-        this.added.emit(this.parent.get('selector').value);
-        this.parent.get('selector').reset({
-            product_id: '',
-            quantity: 10
-        })
-    }
+  onAdd() {
+    this.added.emit(this.parent.get('selector').value);
+    this.parent.get('selector').reset({
+      product_id: '',
+      quantity: 10
+    });
+  }
 }
